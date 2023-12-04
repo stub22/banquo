a9b15cd454c021ad04a90950cdeefcf58f903fe1
stub22 <1082227+stub22@users.noreply.github.com>
2023-12-03 18:26:10 -0800

    Scala 2.13 template code from zio-quickstart-restful-webservice-dockerize

---
+++ banquo_scala/src/main/scala/dev/zio/quickstart/MainApp.scala
@@ -0,0 +1,25 @@
+package dev.zio.quickstart
+
+import dev.zio.quickstart.counter.CounterApp
+import dev.zio.quickstart.download.DownloadApp
+import dev.zio.quickstart.greet.GreetingApp
+import dev.zio.quickstart.users.{InmemoryUserRepo, PersistentUserRepo, UserApp}
+import zio._
+import zio.http._
+
+object MainApp extends ZIOAppDefault {
+  def run = {
+    val httpApps = GreetingApp() ++ DownloadApp() ++ CounterApp() ++ UserApp()
+    Server
+      .serve(httpApps.withDefaultErrorResponse)
+      .provide(
+        Server.defaultWithPort(8080),
+
+        // An layer responsible for storing the state of the `counterApp`
+        ZLayer.fromZIO(Ref.make(0)),
+
+        // To use the persistence layer, provide the `PersistentUserRepo.layer` layer instead
+        InmemoryUserRepo.layer
+      )
+  }
+}
